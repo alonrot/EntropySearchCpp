f03c0b5b1433ddcf3c3f7d73557a3b7f93bb5f8c
Alonso Marco Valle <amarco@tue.mpg.de>
2020-05-30 18:19:30 +0200

    Fancy plotting tool

--- entropy_search/plotting/pyplot_two_subplots.py
+++ entropy_search/plotting/pyplot_two_subplots.py
@@ -26,70 +26,112 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yaml
 import pdb
+import threading
+import logging
+import time
+logger = logging.getLogger(__name__)
 
-def plot_ES( docs ):
 
-	z_plot 	= np.array(docs["z_plot"])
-	f_true 	= np.array(docs["f_true"])
-	mpost 	= np.array(docs["mpost"])
-	stdpost = np.array(docs["stdpost"])
-	Xdata 	= np.array(docs["Xdata"])
-	Ydata 	= np.array(docs["Ydata"])
-	dH_plot = np.array(docs["dH_plot"])
-	EdH_max = np.array(docs["EdH_max"])
-	x_next 	= np.array(docs["x_next"])
-	mu_next = np.array(docs["mu_next"])
+class ReadAndPlot():
 
-	assert z_plot.shape[1] == 1, "This plotting tool is tailored for 1D"
+	def __init__(self,path2data):
+		self.path2data = path2data
+		self.my_node = None
 
-	fig = plt.figure(num=1, figsize=(12, 9), dpi=80, facecolor='w', edgecolor='k')
-	plt.subplot(211)
-	plt.grid(True)
+		hdl_fig = plt.figure(figsize=(12, 8))
+		hdl_fig.suptitle("Bayesian optimization")
+		grid_size = (2,1)
+		self.axes_GPobj = plt.subplot2grid(grid_size, (0,0), colspan=1,fig=hdl_fig)
+		self.axes_acqui = plt.subplot2grid(grid_size, (1,0), colspan=1,fig=hdl_fig)
 
-	Npoints = z_plot[:,0].shape[0]
-	z_plot = z_plot[:,0]
-	if mpost.shape[0] > Npoints:
-		mpost = mpost[0:Npoints]
-		stdpost = stdpost[0:Npoints]
-		f_true = f_true[0:Npoints]
-		dH_plot = dH_plot[0:Npoints]
+		logger.info("Loading plotting data from {0:s} ...".format(self.path2data))
 
-	# GP posterior:
-	plt.plot(z_plot, mpost, 'r', lw=1)
-	plt.fill_between(z_plot, mpost-2*stdpost, mpost+2*stdpost, alpha=0.2, color='r')
+	def plot_ES(self):
 
-	# Prior function:
-	plt.plot(z_plot,f_true,'k--')
+		# Convert to numpy array:
+		z_plot 	= np.array(self.my_node["z_plot"])
+		f_true 	= np.array(self.my_node["f_true"])
+		mpost 	= np.array(self.my_node["mpost"])
+		stdpost = np.array(self.my_node["stdpost"])
+		Xdata 	= np.array(self.my_node["Xdata"])
+		Ydata 	= np.array(self.my_node["Ydata"])
+		dH_plot = np.array(self.my_node["dH_plot"])
+		EdH_max = np.array(self.my_node["EdH_max"])
+		x_next 	= np.array(self.my_node["x_next"])
+		mu_next = np.array(self.my_node["mu_next"])
 
-	# Data:
-	plt.plot(Xdata,Ydata,'bo')
+		assert z_plot.shape[1] == 1, "This plotting tool is tailored for 1D"
 
-	# Next evaluations:
-	plt.plot(x_next,mu_next,'go')
+		# Adjust data shape:
+		Npoints = z_plot[:,0].shape[0]
+		z_plot = z_plot[:,0]
+		if mpost.shape[0] > Npoints:
+			mpost = mpost[0:Npoints]
+			stdpost = stdpost[0:Npoints]
+			f_true = f_true[0:Npoints]
+			dH_plot = dH_plot[0:Npoints]
 
-	plt.title("Entropy Search", fontsize=12)
-	# plt.title("Prior (kernel:  %s)" % kernel, fontsize=12)
+		# GP posterior:
+		self.axes_GPobj.cla()
+		self.axes_GPobj.grid(True)
+		self.axes_GPobj.set_xlim([0,1])
+		self.axes_GPobj.plot(z_plot, mpost, 'r', lw=1)
+		self.axes_GPobj.fill_between(z_plot, mpost-2*stdpost, mpost+2*stdpost, alpha=0.2, color='r')
 
-	plt.subplot(212)
-	plt.grid(True)
-	plt.plot(z_plot, dH_plot, 'b')
-	plt.plot(x_next, EdH_max, 'bo')
+		# Prior function:
+		self.axes_GPobj.plot(z_plot,f_true,'k--')
 
-	plt.show(block=False)
-	plt.pause(0.2)
+		# Data:
+		self.axes_GPobj.plot(Xdata,Ydata,'bo')
 
-# Main:
-current_numiter = 0
-numiter = 0
-while current_numiter < 10 :
+		# Next evaluations:
+		self.axes_GPobj.plot(x_next,mu_next,'go')
 
-	stream 	= open("examples/runES_onedim/output/tmp.yaml", "r")
-	docs 		= yaml.load(stream)
-	numiter = np.array(docs["numiter"])
+		self.axes_GPobj.set_title("Gaussian process", fontsize=12)
+		# plt.title("Prior (kernel:  %s)" % kernel, fontsize=12)
 
-	# Update:
-	if current_numiter != numiter :
-		plot_ES(docs)
-		current_numiter = numiter
+		self.axes_acqui.cla()
+		self.axes_acqui.grid(True)
+		self.axes_acqui.set_xlim([0,1])
+		self.axes_acqui.plot(z_plot, dH_plot, 'b')
+		self.axes_acqui.plot(x_next, EdH_max, 'bo')
+		self.axes_acqui.set_title("Acquisition function", fontsize=12)
+
+		plt.show(block=False)
+		plt.pause(0.5)
+
+	def read_from_yaml(self):
+
+		while True:
+
+			try:
+				stream 	= open(self.path2data, "r")
+				self.my_node = yaml.load(stream,Loader=yaml.UnsafeLoader)
+			except Exception as inst:
+				logger.info("Exception (!) type: {0:s} | args: {1:s}".format(str(type(inst)),str(inst.args)))
+				logger.info("Data corrupted or non-existent!!!")
+				# pdb.set_trace()
+
+			time.sleep(0.5)
+
+	def run(self):
+
+		thread_reading_plotdata = threading.Thread(target=self.read_from_yaml)
+		thread_reading_plotdata.setDaemon(True)
+		thread_reading_plotdata.start()
+		time.sleep(0.5)
+
+		while True:
+			self.plot_ES()
+
+def main():
+
+	rap = ReadAndPlot(path2data="examples/runES_onedim/output/tmp.yaml")
+	rap.run()
+
+
+if __name__ == "__main__":
+
+	main()
 
 
